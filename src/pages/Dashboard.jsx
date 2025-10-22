38fe402c849f3d1354fd853931bd6fc9d7d14cc8
TuanAnhNguyen <nguyentuananhnt04@gmail.com>
2025-10-22 16:50:31 +0700

    frame

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Box ,Typography} from "@mui/material";
+import "@/styles/Dashboard.css"
+
+
+export default function Dashboard(){
+    return(
+    <div className="full">
+     
+    </div>
+
+    
+    )
+}
